9b00aef1a8b8af1f49f836a6e2122f669c985773
Jackie Qiu <jqi26@uclive.ac.nz>
2019-08-29 18:02:00 +1200

    Attempting to establish login, but getting errors in MainApplication

--- src/main/java/kaitech/controller/LogInController.java
+++ src/main/java/kaitech/controller/LogInController.java
@@ -1,8 +1,32 @@
 package kaitech.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+import kaitech.api.model.Business;
+
 /**
  * The controller for the login screen, where the user logs in to gain access to
  * restricted actions.
  */
 public class LogInController {
+    @FXML
+    private Text passwordText;
+
+    @FXML
+    private Text loginButton;
+
+    @FXML
+    private Text loginResponse;
+
+    private Business business;
+
+    public void login() {
+        if (!passwordText.getText().isEmpty()) {
+            boolean loggedIn = business.logIn(passwordText.getText());
+            if (loggedIn) {
+                loginResponse.setText("Log in was successful.");
+            }
+            loginResponse.setVisible(true);
+        }
+    }
 }
