6ad98057b2ac916ae91f76e9e2d02bcd888e712d
Julia Harrison <jrh167@uclive.ac.nz>
2019-09-13 12:42:30 +1200

    Move feature files to correct directory, add blank stepdefs

--- src/test/java/kaitech/RunCucumberTest.java
+++ src/test/java/kaitech/RunCucumberTest.java
@@ -5,8 +5,8 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/kaitech",
-		 glue="kaitech.stepdefs",
+@CucumberOptions(features="src/main/resources/features",
+		 glue="stepdefs",
          plugin={"pretty"},
          snippets = CucumberOptions.SnippetType.CAMELCASE)
 		 
