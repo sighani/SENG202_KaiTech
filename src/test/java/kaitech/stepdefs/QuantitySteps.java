33e7ffa714fde0d51cd23e13183e1b1ec3a088aa
Jackie Qiu <jqi26@uclive.ac.nz>
2019-09-22 15:08:51 +1200

    Fixed the modify ingredient and supplier controllers such that the text and button don't overlap. Fixed some steps in the login steps and quantity steps relating to the business not resetting properly.

--- src/test/java/kaitech/stepdefs/QuantitySteps.java
+++ src/test/java/kaitech/stepdefs/QuantitySteps.java
@@ -41,6 +41,7 @@ public class QuantitySteps {
 
     @And("The business has {int} cheese slices, {int} meat patties, and {int} bun")
     public void theBusinessHasCheeseSlicesMeatPattiesAndBun(Integer int1, Integer int2, Integer int3) {
+        business = BusinessImpl.getInstance();
         BusinessImpl.reset();
         business = BusinessImpl.getInstance();
         business.getInventoryTable().putInventory(cheese, int1);
