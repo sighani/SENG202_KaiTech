e25d7bde868fc5ec35e47d3336727b73afa0e64c
Michael Freeman <michaelfreeman100@gmail.com>
2019-10-05 15:25:50 +1300

    Finished up the model part of the Loyalty card, need a manual creator, db stuff and then adding to sales

--- src/main/java/kaitech/api/model/LoyaltyCard.java
+++ src/main/java/kaitech/api/model/LoyaltyCard.java
@@ -1,5 +1,9 @@
 package kaitech.api.model;
 
+import org.joda.money.Money;
+
+import java.util.Date;
+
 /**
  * Interface to represent loyalty cards
  * @author Michael Freeman
@@ -7,4 +11,56 @@ package kaitech.api.model;
 
 
 public interface LoyaltyCard {
+
+    /**
+     * Adds the appropriate amount of money to the customers card according to
+     * the cost of the purchase, set at 10% currently
+     * @param purchaseCost
+     */
+    void addPoints(Money purchaseCost);
+
+    /**
+     * Takes the current price of the order and returns the new price
+     * after the customers purchase points have been used up
+     * @param purchaseCost
+     * @return newPurchaseCost
+     */
+    Money spendPoints(Money purchaseCost);
+
+    /**
+     * Get the previous date of purchase
+     * @return lastPuchaseDate
+     */
+    Date getLastPurchase();
+
+    /**
+     * Set the previous date of purchase
+     * @param lastPurchase
+     */
+    void setLastPurchase(Date lastPurchase);
+
+    /**
+     * Get the current balance on the card
+     * @return balance
+     */
+    Money getBalance();
+
+    /**
+     * Set the balance on the card
+     * @param balance
+     */
+    void setBalance(Money balance);
+
+    /**
+     * Gets the ID of the current card
+     * @return id
+     */
+    int getId();
+
+    /**
+     * Get the customer name associated with the card
+     * @return customername
+     */
+    String getCustomerName();
 }
+
--- src/main/java/kaitech/model/LoyaltyCardImpl.java
+++ src/main/java/kaitech/model/LoyaltyCardImpl.java
@@ -3,6 +3,7 @@ package kaitech.model;
 import kaitech.api.model.LoyaltyCard;
 import org.joda.money.Money;
 
+import java.math.RoundingMode;
 import java.util.Date;
 
 public class LoyaltyCardImpl implements LoyaltyCard {
@@ -40,11 +41,62 @@ public class LoyaltyCardImpl implements LoyaltyCard {
     public LoyaltyCardImpl(Date currentDate, int id){
         this.lastPurchase = currentDate;
         this.id = id;
+        this.customerName = "Unknown";
+        this.balance = Money.parse("NZD 0.00");
     }
 
+    /*
+    Constructor with customer name
+     */
     public LoyaltyCardImpl(Date currentDate, int id, String customerName){
-
+        this.lastPurchase = currentDate;
+        this.id = id;
+        this.customerName = customerName;
+        this.balance = Money.parse("NZD 0.00");
     }
 
 
+    public void addPoints(Money purchaseCost){
+        //sets last purchase to current date
+        this.lastPurchase = new Date();
+        //update the customers balance by 10 percent of the purchase price
+        this.balance = this.balance.plus(purchaseCost.dividedBy(10, RoundingMode.FLOOR));
+    }
+
+    public Money spendPoints(Money purchaseCost){
+        if(purchaseCost.isLessThan(this.balance)){
+            //the whole card balance is used up, some left over
+            this.balance = this.balance.minus(purchaseCost);
+            return Money.parse("NZD 0.00");
+        }else{
+            //they are both either equal or there is more owed than on the card
+            Money tempPurchaseCost = purchaseCost.minus(this.balance);
+            this.balance = Money.parse("NZD 0.00");
+            return tempPurchaseCost;
+        }
+    }
+
+    public Money getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Money balance) {
+        this.balance = balance;
+    }
+
+    public Date getLastPurchase() {
+        return lastPurchase;
+    }
+
+    public void setLastPurchase(Date lastPurchase) {
+        this.lastPurchase = lastPurchase;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
 }
